2bcd02505156c89213bcbe1ddc82994258403d45
Marcelo Rodrigues <marcelo_rod_@hotmail.com>
2018-05-23 23:13:34 -0300

    Criação da janela de visualização de resumo das atividades do dia

--- src/main/java/com/rod/log/acoes/ResumoAtividades.java
+++ src/main/java/com/rod/log/acoes/ResumoAtividades.java
@@ -2,9 +2,12 @@ package com.rod.log.acoes;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+import com.rod.log.janelas.JanelaResumoDia;
 import com.rod.utils.ArquivoLog;
 
 public class ResumoAtividades implements Runnable {
@@ -17,19 +20,32 @@ public class ResumoAtividades implements Runnable {
 	@Override
 	public void run() {
 		try {
-			new Date();
-			new AnaliseAtividades().analisar(ArquivoLog.obterLogs(Date.from(Instant.now().plus(diasReferencia, ChronoUnit.DAYS))))
-					.stream().forEach(log -> {
-						String descTarefa = log.getTarefa() == null ? "<Sem tarefa no Jira>"
-								: String.format("[%d] %s", log.tarefa.getNumeroTarefa(), log.tarefa.getTitulo());
+			StringBuilder strB = new StringBuilder();
+			Instant dataReferencia = Instant.now().plus(diasReferencia, ChronoUnit.DAYS);
+			strB.append(String.format("Atividades do dia %s:\n",
+					DateTimeFormatter.ofPattern("dd/MM/yyyy").withZone(ZoneId.systemDefault()).format(dataReferencia)));
 
-						System.out.println(String.format("%d min - %s:\n%s", log.getTotalMinutos(), descTarefa,
-								log.getLogSumarizado()));
-					});
+			new AnaliseAtividades().analisar(ArquivoLog.obterLogs(Date.from(dataReferencia))).stream().forEach(log -> {
+				String descTarefa = log.getTarefa() == null ? "<Sem tarefa no Jira>"
+						: String.format("[%d] %s", log.tarefa.getNumeroTarefa(), log.tarefa.getTitulo());
+
+				strB.append(String.format("%s - %s:\n%s\n", descreverTempo(log.getTotalMinutos()), descTarefa,
+						log.getLogSumarizado()));
+			});
+			JanelaResumoDia janela = new JanelaResumoDia();
+			janela.setResumoDia(strB.toString());
+			janela.setVisible(true);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public String descreverTempo(long time) {
+		long horas = time / 60 % 24;
+		long minutos = time % 60;
+		return (horas == 0 ? "" : horas + "h ") + (minutos == 0 ? "" : minutos + "m ")
+				+ (horas == 0 && minutos == 0 ? "<1min " : "");
 	}
 
 }
---
+++ src/main/java/com/rod/log/janelas/JanelaResumoDia.java
@@ -0,0 +1,71 @@
+package com.rod.log.janelas;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class JanelaResumoDia extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+	private static JTextArea textArea;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			JanelaResumoDia dialog = new JanelaResumoDia();
+			dialog.setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public JanelaResumoDia() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			textArea = new JTextArea();
+			contentPanel.add(textArea, BorderLayout.CENTER);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						dispose();
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+		}
+	}
+	
+	public void setResumoDia(String resumoDia){
+		textArea.setText(resumoDia);
+	}
+
+}
